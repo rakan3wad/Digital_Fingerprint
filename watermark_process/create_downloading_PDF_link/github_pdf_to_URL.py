122995c2b232c97d4ddd836c6de817e64c14adfd
rakan3wad <rakan3wad@gmail.com>
2023-08-15 14:17:30 +0300

    first commit

--- watermark_process/create_downloading_PDF_link/github_pdf_to_URL.py
+++ watermark_process/create_downloading_PDF_link/github_pdf_to_URL.py
@@ -1,46 +1,44 @@
-# from github import Github #pip install PyGithub
-# import base64
-
-# GitHub credentials
-TOKEN = "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"
-repo_path = "rakan3wad/digital_fingerprint"
-
 import requests
 import base64
 
-# Replace these with your values
-REPO_OWNER = 'rakan3wad'
-REPO_NAME = 'digital_fingerprint'
-FILE_PATH = 'example.pdf'
-# FILE_NAME = 'yourfil5.pdf' # Name you want to give the file in the repository
-COMMIT_MESSAGE = 'Adding a PDF file'
-
-def create_downloading_PDF_link(FILE_NAME): #EXAMPLE: FILE_NAME = "yourfile4.pdf"
-    # Read the file and encode it in base64
-    with open(FILE_PATH, 'rb') as file:
+def upload_pdf_to_github(token, repo_owner, repo_name, file_path, new_file_name, commit_message):
+    # Open the PDF file and encode it in base64
+    with open(file_path, 'rb') as file:
         content = base64.b64encode(file.read()).decode('utf-8')
 
-    # Prepare the request
-    url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/{FILE_NAME}'
+    # Define the URL for the GitHub API request
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{new_file_name}"
+
+    # Prepare the headers for the request
     headers = {
-        'Authorization': f'token {TOKEN}',
+        'Authorization': f'token {token}',
         'Content-Type': 'application/json'
     }
-    data = {
-        'message': COMMIT_MESSAGE,
-        'content': content
+
+    # Prepare the payload for the request
+    payload = {
+        "message": commit_message,
+        "content": content
     }
 
-    # Send the request
-    response = requests.put(url, json=data, headers=headers)
+    # Make the request to the GitHub API
+    response = requests.put(url, json=payload, headers=headers)
 
-    # Check the response
+    # Check if the request was successful
     if response.status_code == 201:
-        print(f'{FILE_PATH} has been uploaded to {REPO_OWNER}/{REPO_NAME}')
-        return f"https://raw.githubusercontent.com/{REPO_OWNER}/{REPO_NAME}/main/{FILE_NAME}"
+        # Extract the raw URL from the response
+        raw_url = response.json()['content']['download_url']
+        return raw_url
     else:
-        print(f'Error uploading {FILE_PATH}: {response.text}')
-        # Error uploading create_downloading_PDF_link/example.pdf: {"message":"Invalid request.\n\n\"sha\" wasn't supplied.","documentation_url":"https://docs.github.com/rest/repos/contents#create-or-update-file-contents"}
+        return f"Error: {response.json()}"
 
+# Example usage
+token = "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"
+repo_owner = "rakan3wad"
+repo_name = "Digital_Fingerprint"
+file_path = "STAMPED.pdf"
+new_file_name = "watermark_process/create_downloading_PDF_link/all_PDF_files/new_name_for_file3.pdf" #✏️
+commit_message = "Uploading PDF file with new name"
 
-print(create_downloading_PDF_link("STAMPED.pdf")) # حط اسم الكتيب ثم اسم العميل ثم 3 ارقام عشوائية
+raw_url = upload_pdf_to_github(token, repo_owner, repo_name, file_path, new_file_name, commit_message)
+print(raw_url)
